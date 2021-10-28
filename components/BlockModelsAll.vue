d2d8da544c0271783802ab52e5d0fe4e38d219eb
Scrapie-pie <neontec2@gmail.com>
2021-10-28 13:13:01 +0300

    repeated columns support done

--- components/BlockModelsAll.vue
+++ components/BlockModelsAll.vue
@@ -21,6 +21,13 @@
         :image="model.image"
         /> -->
     </div>
+
+    <div class="test-grid">
+      <div class="test-grid__container">
+        <div>qweqweqe</div>
+        <div>qweqweqe</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -44,12 +51,12 @@ export default {
     gap: 30px 35px;
     background-color: blue;
 
-    //@include smart-grid(8); //work
+    @include smart-grid(8); //work
     //@include smart-grid((110px, 9)); //work
     //@include smart-grid((9, 100px)); //work
     //@include smart-grid(200px); //work
     //@include smart-grid((130px, 200px)); // work
-    @include smart-grid((100px, 5, 200px)); // work
+    //@include smart-grid([0.5, 5, 200px]); // work
 
     //@include smart-grid((auto, 7)); //work
     //@include smart-grid((auto, 200px)); // work
@@ -61,8 +68,22 @@ export default {
 
     //@include smart-grid((9, 3)); // Значит 9 колонок, и тянем по 3 каждую -> ДОБАВИТЬ
 
-    //@include smart-grid((auto, 12, 12/8), $mode: 'twelve-grid'); // work
-    //@include smart-grid((130px, 12, 12/6), $mode: 'twelve-per'); // work
+    //@include smart-grid((auto, 12, 12/8), $mode: 'span'); // work
+    //@include smart-grid((130px, 12, 12/6), $mode: 'percent'); // work
+  }
+
+  .test-grid {
+    &__container {
+      margin-top: 100px !important;
+
+      display: grid;
+      gap: 30px 35px;
+      background-color: yellow;
+
+      //@include smart-grid([(100px, 200px), (100px, 200px)]);
+    }
   }
 }
+
+
 </style>
